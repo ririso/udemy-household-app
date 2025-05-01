58ecc7eab1b7246177d8c636a41762882c77c894
ririso <ririsowdev@gmail.com>
2025-05-01 15:52:22 +0900

    firebase実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css"; //利用しない
 import AppLayout from "./components/layout/AppLayout";
+import { db } from "./firebase";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import Report from "./pages/Report";
 import { theme } from "./theme/theme";
+import { Transaction } from "./types";
 
 function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const fecheTransactions = async () => {
+      try {
+        const querySnapShot = await getDocs(collection(db, "Transactions"));
+        // querySnapShot.docs.map((doc) => {
+        //   console.log(doc.id, " => ", doc.data());
+        // });
+
+        const transactionsData = querySnapShot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction;
+        });
+
+        console.log(transactionsData);
+        setTransactions(transactionsData);
+      } catch (err) {
+        // error
+      }
+    };
+    fecheTransactions();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
